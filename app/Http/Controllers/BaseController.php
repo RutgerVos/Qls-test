2f7202a883fa4912bdbc06dec921694912551cf3
Rutger Vos <rutger564@gmail.com>
2024-07-29 15:52:58 +0200

    Started building api call

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class BaseController extends Controller
+{
+    public function index()
+    {
+        // $response = Http::get('https://api.pakketdienstqls.nl/company/9e606e6b-44a4-4a4e-a309-cc70ddd3a103/product');
+        // $response = Http::withBasicAuth('frits@test.qlsnet.nl', '4QJW9yh94PbTcpJGdKz6egwH')
+        // ->get('https://api.pakketdienstqls.nl/company/9e606e6b-44a4-4a4e-a309-cc70ddd3a103/product');
+        // var_dump($response);
+        return view('welcome');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BaseController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [BaseController::class, 'index']);

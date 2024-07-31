f677253af364f26901a687ea3143c425fa4a82d9
Rutger Vos <rutger564@gmail.com>
2024-07-31 14:57:53 +0200

    Added more to api call.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-
+Open in you're code editor terminal and run: php artisan serve.
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -10,10 +10,11 @@ class BaseController extends Controller
 {
     public function index()
     {
+        $client = new \GuzzleHttp\Client(['verify' => false ]);
         // $response = Http::get('https://api.pakketdienstqls.nl/company/9e606e6b-44a4-4a4e-a309-cc70ddd3a103/product');
-        // $response = Http::withBasicAuth('frits@test.qlsnet.nl', '4QJW9yh94PbTcpJGdKz6egwH')
-        // ->get('https://api.pakketdienstqls.nl/company/9e606e6b-44a4-4a4e-a309-cc70ddd3a103/product');
-        // var_dump($response);
+        $response = Http::withBasicAuth('frits@test.qlsnet.nl', '4QJW9yh94PbTcpJGdKz6egwH')
+        ->get('https://api.pakketdienstqls.nl/company/9e606e6b-44a4-4a4e-a309-cc70ddd3a103/product');
+        var_dump($response);
         return view('welcome');
     }
 }

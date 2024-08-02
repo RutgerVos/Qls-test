f8430d3ea1dad39d83a44969b4289f7f0ac7faeb
Rutger Vos <rutger564@gmail.com>
2024-08-02 15:26:11 +0200

    Api call now works as intended.
    Also expanded readme.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Open in you're code editor terminal and run: php artisan serve.
+Go to localhost: http://127.0.0.1:8000/
+Click on the button Create pdf.
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -5,16 +5,18 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Spatie\LaravelPdf\Facades\Pdf;
 
 class BaseController extends Controller
 {
     public function index()
     {
+        // Pdf::html('<h1>Hello world!!</h1>')->save('/some/directory/invoice.pdf');
         $client = new \GuzzleHttp\Client(['verify' => false ]);
         // $response = Http::get('https://api.pakketdienstqls.nl/company/9e606e6b-44a4-4a4e-a309-cc70ddd3a103/product');
         $response = Http::withBasicAuth('frits@test.qlsnet.nl', '4QJW9yh94PbTcpJGdKz6egwH')
-        ->get('https://api.pakketdienstqls.nl/company/9e606e6b-44a4-4a4e-a309-cc70ddd3a103/product');
-        var_dump($response);
+        ->get('https://api.pakketdienstqls.nl/companies/9e606e6b-44a4-4a4e-a309-cc70ddd3a103/brands/e41c8d26-bdfd-4999-9086-e5939d67ae28')->json();
+        var_dump($response['data']);
         return view('welcome');
     }
 }

bf8e0cbf0270f9f4562d3183a525c2f7151a779e
Rutger Vos <rutger564@gmail.com>
2024-08-20 14:33:36 +0200

    Made it possible to download pdf
    Also pdf now has some api within it.

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Http;
 use function Spatie\LaravelPdf\Support\pdf;
 use Spatie\LaravelPdf\Facades\Pdf;
+use File;
 
 class BaseController extends Controller
 {
@@ -14,17 +15,21 @@ class BaseController extends Controller
         $client = new \GuzzleHttp\Client(['verify' => false ]);
         $response = Http::withBasicAuth('frits@test.qlsnet.nl', '4QJW9yh94PbTcpJGdKz6egwH')
         ->get('https://api.pakketdienstqls.nl/companies/9e606e6b-44a4-4a4e-a309-cc70ddd3a103/brands/e41c8d26-bdfd-4999-9086-e5939d67ae28');
-        // var_dump($response['data']);
-        return view('welcome', ['reponse' => $response]);
+        var_dump($response['data']);
+        return view('welcome', ['response' => $response['data']]);
     }
 
     public function pdf()
     {
         $response = Http::withBasicAuth('frits@test.qlsnet.nl', '4QJW9yh94PbTcpJGdKz6egwH')
         ->get('https://api.pakketdienstqls.nl/companies/9e606e6b-44a4-4a4e-a309-cc70ddd3a103/brands/e41c8d26-bdfd-4999-9086-e5939d67ae28');
-     Pdf::view('pdf', ['reponse' => $response])
+     Pdf::view('pdf', ['response' => $response['data']])
     ->format('a4')
     ->save('test.pdf');
-    return var_dump('pdf made');
+    $file = public_path(). "/test.pdf";
+     $headers = [
+        'Content-Type' => 'application/pdf',
+     ];
+    return response()->download($file, 'test.pdf', $headers);
     }
 }
--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -8,4 +8,25 @@
 <div>
     Page 2
 </div> --}}
-<h1>test</h1>
+<p>id = {{ $response['id'] }}</p>
+<p>company_id = {{ $response['company_id'] }}</p>
+<a>name = {{ $response['name'] }}</a>
+<p>sender_contact_id = {{ $response['sender_contact_id'] }}</p>
+<p>return_contact_id = {{ $response['return_contact_id'] }}</p>
+<p>tracking_notification_policy = {{ $response['tracking_notification_policy'] }}</p>
+<p>tracking_first_notification_on = {{ $response['tracking_first_notification_on'] }}</p>
+<p>fulfillment_allow_auto_print = {{ $response['fulfillment_allow_auto_print'] }}</p>
+<p>fulfillment_allow_partical_delivery = {{ $response['fulfillment_allow_partical_delivery'] }}</p>
+<p>fulfillment_deliveryslip_bottom_text = {{ $response['fulfillment_deliveryslip_bottom_text'] }}</p>
+<p>fulfillment_deliveryslip_mode = {{ $response['fulfillment_deliveryslip_mode'] }}</p>
+<p>fulfillment_force_servicepoint = {{ $response['fulfillment_force_servicepoint'] }}</p>
+<p>fulfillment_myorder_allow_servicepoint = {{ $response['fulfillment_myorder_allow_servicepoint'] }}</p>
+<p>fulfillment_myorder_allow_timeframe = {{ $response['fulfillment_myorder_allow_timeframe'] }}</p>
+<p>fulfillment_pack_deliveryslip = {{ $response['fulfillment_pack_deliveryslip'] }}</p>
+<p>fulfillment_pack_deliveryslip_ondemand = {{ $response['fulfillment_pack_deliveryslip_ondemand'] }}</p>
+<p>return_within_days = {{ $response['return_within_days'] }}</p>
+<p>return_action_after_delivery_fail = {{ $response['return_action_after_delivery_fail'] }}</p>
+<p>return_action_after_retour_term = {{ $response['return_action_after_retour_term'] }}</p>
+<p>return_action_default = {{ $response['return_action_default'] }}</p>
+<p>with_return_label = {{ $response['with_return_label'] }}</p>
+<p>created_at = {{ $response['created'] }}</p>
